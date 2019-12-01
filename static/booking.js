d45b5f72013efcde550fc86b7b16b75d56560eb2
Maryfrances-U <55061982+Maryfrances-U@users.noreply.github.com>
2019-11-30 21:49:58 -0500

    CSS & JavaScript

---
+++ static/booking.js
@@ -0,0 +1,78 @@
+function makebooking(spotname) {
+    $.ajax({
+        "url": "/actualbooking",
+        "data": {
+            "spotname": spotname,
+            "date": $('#dateCalender').val(),
+		    "stime": $('#starttime').val(),
+		    "etime": $('#endtime').val(),
+        },
+        "type": "POST",
+        "dataType": "json"
+	}).done(function (response){
+	    console.log(response);
+    }).fail(function(xhr, status, description){
+    		console.log("Error: " + status + description);
+	});
+}
+
+function changepref() {
+    $.ajax({
+        "url": "/selection",
+        "data": {
+            "date": $('#dateCalender').val(),
+		    "stime": $('#starttime').val(),
+		    "etime": $('#endtime').val(),
+            "food" : $('#foodfilter').prop('checked'),
+            "quiet": $('#quietfilter').prop('checked'),
+            "whisper": $('#whisperfilter').prop('checked'),
+            "comp": $('#compfilter').prop('checked'),
+        },
+        "type": "POST",
+        "dataType": "json"
+	}).done(function (response){
+	    console.log("Successfully rechose");
+      console.log(response);
+	    let avspots = response.availablespots;
+	    let currOptions = $("#formspotcontainer");
+            currOptions.empty();
+
+        //creates a new radio input with the spot's name next to it
+	    for(let opt of avspots) {
+	        let label = document.createElement("label");
+		    let r = document.createElement("input");
+		    let text = document.createElement("label");
+		    text.textContent=opt;
+
+		    r.setAttribute("type", "radio");
+		    r.name = "spot";
+		    label.appendChild(r);
+		    label.appendChild(text);
+		    currOptions.append(label);
+		    let linebreak1 = document.createElement("br");
+		    currOptions.append(linebreak1);
+		    let linebreak2 = document.createElement("br");
+		    currOptions.append(linebreak2);
+
+	    }
+	}).fail(function(xhr, status, description){
+    		console.log("Error: " + status + description);
+	});
+}
+
+function openForm() {
+    let radios = document.getElementsByName('spot');
+
+    for(let i = 0; i < radios.length; i++) {
+        if(radios[i].checked) {
+            makebooking(radios[i].nextSibling.textContent);
+        }
+    }
+
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("popup").style.display = "none";
+}
